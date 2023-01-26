35c61eabb2e26feedc1be428933983fbda00c5f7
nepsoul <nirumagar901@gmail.com>
2023-01-26 21:52:49 +0545

    EX-6.19: modification of Notification component that uses connect func instead of hook api

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,18 +1,25 @@
-import { useSelector } from "react-redux";
+//import { useSelector } from "react-redux";
+import { connect } from "react-redux";
 
-const Notification = () => {
-  const notifications = useSelector((state) => state.notification);
-  if (!notifications) {
+const Notification = (props) => {
+  //const notifications = useSelector((state) => state.notification);
+  //console.log("notifications", notification);
+
+  if (!props.notification) {
     return null;
   }
-  //console.log("notifications", notifications);
 
   const style = {
     border: "solid",
     padding: 10,
     borderWidth: 1,
   };
-  return <div style={style}>{notifications}</div>;
+  return <div style={style}>{props.notification}</div>;
 };
 
-export default Notification;
+const mapStateToProps = (state) => {
+  return { notification: state.notification };
+};
+
+const ConnectedAnecdotes = connect(mapStateToProps)(Notification);
+export default ConnectedAnecdotes;
